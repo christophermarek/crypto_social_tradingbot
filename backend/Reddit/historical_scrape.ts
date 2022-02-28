7a06bc8c0bd6d046f0ac336bbc75f1d584c28844
christophermarek <christophermarek42@gmail.com>
2022-02-28 13:35:57 -0500

    fetch historical data template done

--- backend/Reddit/historical_scrape.ts
+++ backend/Reddit/historical_scrape.ts
@@ -28,14 +28,51 @@ const main = async () => {
 
     // fetch the after field and loop queries
 
-    let responses = []
-    for(let i = 0; i < 25; i++){
-       
-        const first_page = await axios.get(
-            `https://old.reddit.com/r/cryptocurrency/comments.json?after=t1_hyt1ymm`
-        );
+    let after = first_after;
+
+    let commentsList = [];
+
+    // append first response
+    const comments = resp.data.children;
+    let size = comments.length;
+    for (let i = 0; i < size; i++) {
+        const comment_data: api_comment_data = comments[i].data;
+        commentsList.push(comment_data)
     }
 
+    // now go back 25 pages
+    for (let i = 0; i < 25; i++) {
+
+        const page_fetched = await axios.get(
+            `https://old.reddit.com/r/cryptocurrency/comments.json?after=${after}`
+        );
+
+        const resp: RootObject = page_fetched.data;
+
+        after = resp.data.after
+
+        const comments = resp.data.children;
+        let size = comments.length;
+        for (let i = 0; i < size; i++) {
+            const comment_data: api_comment_data = comments[i].data;
+            commentsList.push(comment_data)
+        }
+
+        // clean this response now into a proper obj
+
+    }
+
+    // now that this works I need to do better data processing so I can see how far back I can go
+    // important to track the dates
+    // 
+
+    // save to file 
+    var fs = require('fs');
+    fs.writeFile("test.txt", JSON.stringify(commentsList), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 
 }
 
